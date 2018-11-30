168af645a5cc13f9adacd7843f1b1e270c956db1
Popov Timofei <P_timofei@mail.ru>
2018-11-30 13:01:19 +0300

    added recipe search by N ingredients and return recipeName and recipeInstruction

--- src/main/java/server/FromDB.java
+++ src/main/java/server/FromDB.java
@@ -3,13 +3,15 @@ package server;
 import java.sql.*;
 import java.util.ArrayList;
 
+
 public class FromDB {
     private static final String URL = "jdbc:mysql://localhost:3306/recipesdb?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true" + "&useLegacyDatetimeCode=false&serverTimezone=UTC";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "1002199Timka";
     public String name;
 
-    private static final String SELECT = "select recipeName from recipelist where recipe_ID IN (select recipe_ID from ingred group by recipe_ID having count(1) = sum(ingredient in(?,?,?,?,?,?,?)))";
+
+    private static final String SELECT = "select recipeName,recipeInstruction from recipelist where recipe_ID IN (select recipe_ID from ingred group by recipe_ID having count(1) = sum(ingredient in(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)))";
 
     Connection connection = null;
 
@@ -27,21 +29,33 @@ public class FromDB {
 
     public ArrayList<String> GetFromDB(String ingredient) {
         PreparedStatement preparedStatement;
-        String[] ingredients = ingredient.split(";");
+
+        String[] ingreds = ingredient.split(";");
+        ArrayList<String> ingredients = new ArrayList<String>();
+        int j = ingreds.length;
+
+        for (int i = 0; i < j; i++) {
+            ingredients.add(i,ingreds[i]);
+        }
+        for (int i = j; i < 20; i++) {
+            ingredients.add(i, "");
+        }
+
+
         try {
+
             preparedStatement = connection.prepareStatement(SELECT);
-            preparedStatement.setString(1, ingredients[0]);
-            preparedStatement.setString(2, ingredients[1]);
-            preparedStatement.setString(3, ingredients[2]);
-            preparedStatement.setString(4, ingredients[3]);
-            preparedStatement.setString(5, ingredients[4]);
-            preparedStatement.setString(6, ingredients[5]);
-            //preparedStatement.setString(7, ingredients[6]);
+
+            for (int i = 0; i < 20; i++) {
+                preparedStatement.setString(i+1, ingredients.get(i));
+            }
+
             ResultSet recipe = preparedStatement.executeQuery();
             ArrayList<String> names = new ArrayList<String>();
 
+
             while (recipe.next()) {
-                name = recipe.getString("recipeName");
+                name =  recipe.getString("recipeName") + ";" + recipe.getString("recipeInstruction")  + ";";
                 names.add(name);
             }
             return names;
